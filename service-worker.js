969f79306de9b842e180ba9a374c218b6d42d888
govinhood <65528478+govinhood@users.noreply.github.com>
2026-07-05 22:54:32 +0545

    Revert "Merge branch 'main' of https://github.com/govinhood/r"
    This reverts commit 9de7d8e530aee540bcf831134c589b5734899155, reversing
    changes made to 209467cfe3f120a69bb001547926746dd517d225.

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,5 @@
-
 const CACHE_NAME='gfm-v1';
 const urlsToCache=['/r/','/r/index.html','/r/style.css','/r/script.js','/r/assets/icon-192.png','/r/assets/icon-512.png','/r/assets/gfmlcct.png','/r/assets/gfmst.png','/r/assets/gfmc.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)));self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
-
-self.addEventListener('install', event => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {});
-
